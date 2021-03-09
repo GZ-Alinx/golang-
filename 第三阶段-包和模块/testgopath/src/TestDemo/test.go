44748f218951908faeb9600da569e84518ea98cb
alinx <itadminlx@163.com>
2021-03-09 23:19:04 +0800

    go 包学习 gopath+vendor方式

---
+++ 第三阶段-包和模块/testgopath/src/TestDemo/test.go
@@ -0,0 +1,7 @@
+package tests
+
+import "fmt"
+
+func TestsD() {
+	fmt.Println("88888888888888888")
+}
---
+++ 第三阶段-包和模块/testgopath/src/mian.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"TestDemo"	"fmt"
+)
+
+
+func main() {
+	fmt.Println(TestDemo.TestsD())
+	fmt.println("sssssss")
+}
