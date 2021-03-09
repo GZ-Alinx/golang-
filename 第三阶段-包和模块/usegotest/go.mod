b1ecbc2ab0245a725837621b10ca44e4b4e9b946
alinx <itadminlx@163.com>
2021-03-09 23:18:30 +0800

    go 包学习 使用第三方包

---
+++ 第三阶段-包和模块/usegotest/go.mod
@@ -0,0 +1,5 @@
+module usegotest
+
+go 1.14
+
+require github.com/GZ-Alinx/gotest v0.0.0-20210309141440-f657e51ecb9f // indirect
---
+++ 第三阶段-包和模块/usegotest/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/GZ-Alinx/gotest"
+)
+
+func main() {
+	fmt.Println(gotest.Add(1, 2))
+}
